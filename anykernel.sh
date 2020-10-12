6c45f8eceb09a7d5301004f1f2384aa03c87f001
lyceris-chan <ghostdrain@outlook.com>
2020-10-12 15:44:20 +0200

    CONSUME THE PAIMON

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy kernel by Lacia-chan
+kernel.string=Sleepy kernel ~ But Paimon is emergency food (By Lacia_chan)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
